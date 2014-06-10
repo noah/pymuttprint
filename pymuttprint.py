fa271bef78a9d21ba30fc2c7258fb5df7d2c06b7
Noah Tilton <noah@tilton.co>
2014-06-10 12:58:34 -0500

    more accurate formatting - regex to replace newlines with LaTeX linebreaks

--- pymuttprint.py
+++ pymuttprint.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import re
 import sys
 import email
 from os import chdir
@@ -21,39 +22,40 @@ for part in m.walk():
     ct = part.get_content_type()
     cd = part.get('Content-Disposition')
     if ct == 'text/plain':
-        body += '\n'.join(part.get_payload(decode=True).splitlines(False))
+        body += part.get_payload(decode=True)
     else:
         fn = part.get_filename()
         if fn is not None: attachments.append( fn )
 
-# TEX_REPLACE = { # not an exhaustive list
-#         "<" : r"\textless ",
-#         ">" : r"\textgreater ",
-#         "&" : r"\&",
-#         "$" : r"$",
-#         "#" : r"\#",
-#         "%" : r"\%",
-#         "_" : r"\_",
-#         "{" : r"\{",
-#         "}" : r"\}",
-#         r"\\" : r"\\ \relax " # see: http://tex.stackexchange.com/questions/64098/brackets-in-first-column-in-table-give-missing-number-treated-as-zero-error
-# }
-#
-# from textwrap import wrap
+TEX_REPLACE = { # not an exhaustive list
+        "<" : r"\textless ",
+        ">" : r"\textgreater ",
+        "&" : r"\&",
+        "$" : r"\$",
+        "#" : r"\#",
+        "%" : r"\%",
+        "_" : r"\_",
+        "{" : r"\{",
+        "}" : r"\}",
+        "^" : r"^{}",
+        #r"\\" : r"\\ \relax " # see: http://tex.stackexchange.com/questions/64098/brackets-in-first-column-in-table-give-missing-number-treated-as-zero-error
+}
 
 
-#def texify(s):
-#    for c in TEX_REPLACE.keys():
-#        s = '  '.join(wrap(s.replace(c, TEX_REPLACE[c]), 50))
-#    return s
+def texify(s):
+    for c in TEX_REPLACE.keys():
+        s = s.replace(c, TEX_REPLACE[c])
+    return s
 
 to      = m.get('to','')
 date    = m.get('date','')
 ffrom   = m.get('from','')
 subject = m.get('subject','')
-body    = body
+body    = texify( re.sub(r'^(.+)\n|\r|\n\r$', r"\1\\\\", body, flags=re.MULTILINE) )
+
 #
-# attachments = [texify(a) for a in attachments]
+attachments = [texify(a) for a in attachments]
+
 template    = open(join(BASEDIR, 'template.tex'), 'r').read() % (
     date,
     to,
@@ -67,7 +69,11 @@ template    = open(join(BASEDIR, 'template.tex'), 'r').read() % (
 p       = Popen(['xelatex'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 tex_log = p.communicate(input=template)[0]
 
+if p.returncode > 0:
+    print( tex_log)
+exit(p.returncode)
+
 # for debugging:
 #print m
-#print( tex_log)
+#print body
 #print template
